21823105da54b96b3d8e8c564761dd76d03eae32
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2006-04-25 17:53:54 +0000

    agora sim validacao dentro do modelo de cnpj

--- test/cnpj_test.rb
+++ test/cnpj_test.rb
@@ -1,16 +1,17 @@
+
 require File.dirname(__FILE__) + '/abstract_unit'
 require '../lib/validates_as_cnpj'
 
 # Modelo
 class CNPJData < ActiveRecord::Base
   set_table_name "cnpjs"
+
+  validates_as_cnpj :cnpj
 end
 
 # Testes
 class CNPJsTest < Test::Unit::TestCase
   def test_cnpj_invalido
-    CNPJData.validates_as_cnpj :cnpj
-
     cnpj_invalido = CNPJData.create(:id => 1, :cnpj => "51.357.999/1110-98")
     cnpj_invalido.save
 
@@ -18,8 +19,6 @@ class CNPJsTest < Test::Unit::TestCase
   end
 
   def test_cnpj_valido
-    CNPJData.validates_as_cnpj :cnpj
-
     cnpj_valido = CNPJData.create(:id => 1, :cnpj => "94.132.024/0001-48")
     cnpj_valido.save
 
