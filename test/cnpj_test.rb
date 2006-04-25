9e8d1524d5a364de4594e3f24322a9e58a3f404f
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2006-04-25 17:51:53 +0000

    validacao eh feita dentro do modelo arrumei os asserts pra verificar se o registros sao salvos

--- test/cnpj_test.rb
+++ test/cnpj_test.rb
@@ -14,8 +14,7 @@ class CNPJsTest < Test::Unit::TestCase
     cnpj_invalido = CNPJData.create(:id => 1, :cnpj => "51.357.999/1110-98")
     cnpj_invalido.save
 
-    assert ( cnpj_invalido.errors.on(:cnpj) != nil ),
-             "CNPJ invalido foi salvo."
+    assert ( not cnpj_invalido.save ), "CNPJ invalido foi salvo."
   end
 
   def test_cnpj_valido
@@ -24,7 +23,6 @@ class CNPJsTest < Test::Unit::TestCase
     cnpj_valido = CNPJData.create(:id => 1, :cnpj => "94.132.024/0001-48")
     cnpj_valido.save
 
-    assert ( cnpj_valido.errors.on(:cnpj) == nil ),
-             "CNPJ valido nao foi salvo."
+    assert ( cnpj_valido.save ), "CNPJ valido nao foi salvo."
   end
 end
