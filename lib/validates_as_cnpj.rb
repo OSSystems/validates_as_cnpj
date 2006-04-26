2797a99b4b8da409109457621c611d05478a17cd
Otávio Salvador <otavio@ossystems.com.br>
2006-04-26 22:08:05 +0000

    - Adicionada licensa nos arquivos;  - Adicionado final de linha nos LEIAME's

--- lib/validates_as_cnpj.rb
+++ lib/validates_as_cnpj.rb
@@ -1,3 +1,19 @@
+# validates_as_cnpj.rb - implement the validation of cnpj
+#  
+#  Copyright (c) 2006 O.S. Systems
+#  
+#  Authors: André Ribeiro Camargo <andre@boaideia.inf.br>,
+#           Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
+# 
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
 module ValidaCNPJ
   def self.valida_cnpj(cnpj = nil)
     return nil if cnpj.nil?
