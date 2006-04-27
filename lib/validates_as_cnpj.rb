ef113a9b238a9bc5393aba2e4fd008210f61426a
André Camargo <andre@boaideia.inf.br>
2006-04-27 03:05:08 +0000

    modifiquei schema da fixture para aceitar cnpj com null (algumas aplicacoes podem ter cnpj opcional)
    inclui alguns testes e substitui #create por #new
    
    refatorei algumas coisinhas poucas no algoritmo de validacao
    
    troquei mensagem de erro para ingles (temos que encontrar outra forma de traduzir isso, globalize?)

--- lib/validates_as_cnpj.rb
+++ lib/validates_as_cnpj.rb
@@ -29,33 +29,29 @@ module ValidaCNPJ
                99999999999999
                00000000000000}
 
-    valor = cnpj.scan /[0-9]/
+    valor = cnpj.scan(/[0-9]/).collect{|x| x.to_i}
 
     fatores = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
 
-    if valor.length == 14
-      unless nulos.member? valor.join
-        valor = valor.collect{|x| x.to_i}
-        
+    if valor.length == 14 and not nulos.member?(valor.to_s)
+      soma = 0
+      0.upto(11) do |i|
+        soma += valor[i] * fatores[i]
+      end
+
+      soma = soma - (11 * (soma / 11))       
+      dv1 = soma < 2 ? 0 : 11 - soma
+
+      if dv1 == valor[12]
         soma = 0
-        0.upto(11) do |i|
-          soma += valor[i] * fatores[i]
+        0.upto(12) do |i|
+          soma += valor[i] * (i == 0 ? 6 : fatores[i-1])
         end
 
-        soma = soma - (11 * (soma / 11))       
-        dv1 = soma < 2 ? 0 : 11 - soma
+        soma = soma - (11 * (soma /11))
+        dv2 = soma < 2 ? 0 : 11 - soma
 
-        if dv1 == valor[12]
-          soma = 0
-          0.upto(12) do |i|
-            soma += valor[i] * (i == 0 ? 6 : fatores[i-1])
-         end
-
-          soma = soma - (11 * (soma /11))
-          dv2 = soma < 2 ? 0 : 11 - soma
-
-          return true if dv2 == valor[13]
-        end
+        return true if dv2 == valor[13]
       end
     end
     return nil
@@ -66,13 +62,11 @@ module ActiveRecord
   module Validations
     module ClassMethods
       def validates_as_cnpj(*attr_names)
-        configuration = { :message => "cnpj inválido" }
+        configuration = { :message => "is invalid" }
         configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
 
         validates_each(attr_names, configuration) do |record, attr_name, value|
-          value_before_type_cast = record.send("#{attr_name}_before_type_cast")
-        
-          next if value.empty?
+          next if value.blank?
 
           unless ValidaCNPJ::valida_cnpj(value)
             record.errors.add(attr_name, configuration[:message])
