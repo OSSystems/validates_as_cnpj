801dbbd487c9e8f767f964ef680ee0ffc094eddb
Vinicius Alves Hax <viniciushax@gmail.com>
2008-04-05 13:43:05 +0000

    * Resolvendo problemas de erro ao rodar os testes, pois a localizacao dos diretorios nao estava sendo resolvida adequadamente  * Criacao de campo id como chave primaria nula gerava um erro no sqlite e nao influencia em nada na execucao dos testes, logo foi removida

--- test/abstract_unit.rb
+++ test/abstract_unit.rb
@@ -1,7 +1,7 @@
-require '../../../../config/environment'
+require File.dirname(__FILE__) + '/../../../../config/environment'
 
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 
 require 'test/unit'
 require 'active_record'
-require 'connection'
+require File.dirname(__FILE__) + '/connection'
--- test/cnpj_test.rb
+++ test/cnpj_test.rb
@@ -1,6 +1,5 @@
-
 require File.dirname(__FILE__) + '/abstract_unit'
-require '../lib/validates_as_cnpj'
+require File.dirname(__FILE__) + '/../lib/validates_as_cnpj'
 
 # Modelo
 class CNPJData < ActiveRecord::Base
